c07fc631c145d3ce2225114a57f724910ef859bb
Makmaer <instrojgrad@gmail.com>
2022-06-22 23:50:58 +0500

    vtoray zadzcha gotova!

--- Zadacha2_/Program.cs
+++ Zadacha2_/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int num1, num2, num3, max;
+string p;
+Console.Write("Neobhodimo vvesti tri raznih chisla ");
+
+Console.Write("Vvedite pervoe chislo : ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Vvedite vtoroe chislo : ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Vvedite pervoe chislo : ");
+num3 = Convert.ToInt32(Console.ReadLine());
+
+max = num1;
+if (max>num2)
+{   if (max>num3)
+    { Console.WriteLine("maksimalnoe chislo= " + max); }
+    else 
+    {max=num3;
+    Console.WriteLine("maksimalnoe chislo= " + max);}
+}
+else   if (max<num2) 
+{  max = num2;
+   if (max>num3)
+   {Console.WriteLine("maksimalnoe chislo= " + max);}
+   else {max = num3;
+   Console.WriteLine("maksimalnoe chislo= " + max);}
+}
+else 
+
+{  Console.WriteLine("vvedeni odinakovie chisla");
+}
+
