b9105c0f34e20997f80d122e2d5f6e30ebeaa89c
Makmaer <instrojgrad@gmail.com>
2022-06-22 23:13:34 +0500

    Zadacha 1 gotova!

--- Zadacha1_sravnenieDvuhChisel/Program.cs
+++ Zadacha1_sravnenieDvuhChisel/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int num1, num2;
+string p;
+Console.Write("Vvedite pervoe chislo : ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Vvedite vtoroe chislo : ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1>num2)
+{   p="> ";
+}
+else 
+  if (num1<num2) 
+{   p="< ";
+}
+else 
+
+{   p="= ";
+}
+
+Console.WriteLine("pervoe chislo " + p + "vtoroe chislo");
