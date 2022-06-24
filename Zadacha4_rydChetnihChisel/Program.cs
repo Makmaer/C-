70d4b0445b45c4ce17ab2a81991fa3ae1300344a
Makmaer <instrojgrad@gmail.com>
2022-06-24 20:15:36 +0500

    zadacha na postroenie ryda chetnih chisel gotova

---
+++ Zadacha4_rydChetnihChisel/Program.cs
@@ -0,0 +1,15 @@
+﻿int N;
+Console.Write("Vvedite chislo : ");
+N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Polnij ryd: ");
+for (int x = 1; x < N+1; x++)
+{
+Console.Write(" " + x);
+}
+Console.WriteLine( "\n Ryd iz chetnih: ");
+
+for (int x=2; x < N+1;)
+{
+Console.Write(" " + x);
+x=x+2;
+}
