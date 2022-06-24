5cf07ff5b87cf6fb3ab24c06eee3e8644ba154e8
Makmaer <instrojgrad@gmail.com>
2022-06-24 19:37:11 +0500

    kommitim papku s pervoy zadachey

---
+++ Zadacha3_opredelenieChetnosti/Program.cs
@@ -0,0 +1,15 @@
+﻿int num1;
+string p;
+Console.Write("Vvedite chislo : ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 % 2 == 0)
+{   p="chetnoe ";
+}
+
+else 
+
+{   p="nechetnoe ";
+}
+
+Console.WriteLine("chislo " + num1 + " " + p);
